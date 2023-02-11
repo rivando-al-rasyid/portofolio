a6c1edc5e7f9244f8e94ef0b0db2f5344f6b80dc
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2023-02-11 16:16:18 +0700

    reset

--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -1,101 +1,114 @@
 <!doctype html>
 <html class="no-js" lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>@yield('title')</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/assets/img/favicon.png') }}">
-        <!-- Place favicon.ico in the root directory -->
-
-		<!-- CSS here -->
-        <link rel="stylesheet" href="{{ asset('frontend/assets/css/bootstrap.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('frontend/assets/css/animate.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('frontend/assets/css/magnific-popup.css') }}">
-        <link rel="stylesheet" href="{{ asset('frontend/assets/css/fontawesome-all.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('frontend/assets/css/slick.css') }}">
-        <link rel="stylesheet" href="{{ asset('frontend/assets/css/default.css') }}">
-        <link rel="stylesheet" href="{{ asset('frontend/assets/css/style.css') }}">
-        <link rel="stylesheet" href="{{ asset('frontend/assets/css/responsive.css') }}">
-
-         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+<head>
 
 
-    </head>
-    <body>
+    <!-- CSS FILES -->
 
-        <!-- preloader-start -->
-        <div id="preloader">
-            <div class="rasalina-spin-box"></div>
-        </div>
-        <!-- preloader-end -->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-		<!-- Scroll-top -->
-        <button class="scroll-top scroll-to-target" data-target="html">
-            <i class="fas fa-angle-up"></i>
-        </button>
-        <!-- Scroll-top-end-->
+    <title>@yield('title')</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- header-area -->
-     @include('frontend.body.header')
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/assets/img/favicon.png') }}">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+
+    <link href="{{ asset('frontend/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('frontend/assets/css/bootstrap-icons.css') }}" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/magnific-popup.css') }}">
+
+    <link href="{{ asset('frontend/assets/css/aos.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('frontend/assets/css/templatemo-nomad-force.css') }}" rel="stylesheet">
+    <!--
+
+TemplateMo 567 Nomad Force
+
+https://templatemo.com/tm-567-nomad-force
+
+-->
+    <!--
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/fontawesome-all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/default.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/responsive.css') }}"> -->
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
+
+
+</head>
+
+<body>
+    <!-- header-area -->
+    @include('frontend.body.header')
     <!-- header-area-end -->
 
     <!-- main-area -->
     <main>
         @yield('main')
 
-   </main>
-        <!-- main-area-end -->
+    </main>
+    <!-- main-area-end -->
 
 
 
-        <!-- Footer-area -->
-        @include('frontend.body.footer')
-        <!-- Footer-area-end -->
+    <!-- Footer-area -->
+    @include('frontend.body.footer')
+    <!-- Footer-area-end -->
 
 
+    <!-- JS here -->
+    <script src="{{ asset('frontend/assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/jquery.sticky.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/aos.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/magnific-popup-options.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/scrollspy.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/custom.js') }}"></script>
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+    <script>
+        @if(Session::has('message'))
+        var type = "{{ Session::get('alert-type','info') }}"
+        switch (type) {
+            case 'info':
+                toastr.info(" {{ Session::get('message') }} ");
+                break;
+
+            case 'success':
+                toastr.success(" {{ Session::get('message') }} ");
+                break;
+
+            case 'warning':
+                toastr.warning(" {{ Session::get('message') }} ");
+                break;
+
+            case 'error':
+                toastr.error(" {{ Session::get('message') }} ");
+                break;
+        }
+        @endif
+    </script>
 
 
-		<!-- JS here -->
-        <script src="{{ asset('frontend/assets/js/vendor/jquery-3.6.0.min.js') }}"></script>
-        <script src="{{ asset('frontend/assets/js/bootstrap.min.js') }}"></script>
-        <script src="{{ asset('frontend/assets/js/isotope.pkgd.min.js') }}"></script>
-        <script src="{{ asset('frontend/assets/js/imagesloaded.pkgd.min.js') }}"></script>
-        <script src="{{ asset('frontend/assets/js/jquery.magnific-popup.min.js') }}"></script>
-        <script src="{{ asset('frontend/assets/js/element-in-view.js') }}"></script>
-        <script src="{{ asset('frontend/assets/js/slick.min.js') }}"></script>
-        <script src="{{ asset('frontend/assets/js/ajax-form.js') }}"></script>
-        <script src="{{ asset('frontend/assets/js/wow.min.js') }}"></script>
-        <script src="{{ asset('frontend/assets/js/plugins.js') }}"></script>
-        <script src="{{ asset('frontend/assets/js/main.js') }}"></script>
+</body>
 
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-
-<script>
- @if(Session::has('message'))
- var type = "{{ Session::get('alert-type','info') }}"
- switch(type){
-    case 'info':
-    toastr.info(" {{ Session::get('message') }} ");
-    break;
-
-    case 'success':
-    toastr.success(" {{ Session::get('message') }} ");
-    break;
-
-    case 'warning':
-    toastr.warning(" {{ Session::get('message') }} ");
-    break;
-
-    case 'error':
-    toastr.error(" {{ Session::get('message') }} ");
-    break; 
- }
- @endif 
-</script>
-
-
-    </body>
 </html>
